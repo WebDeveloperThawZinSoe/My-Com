a48d85636e8b809c6e2b6a2dd7f2f9860619926a
Thaw Zin Soe <thawzinsoe.dev@gmail.com>
2022-10-30 15:22:47 +0630

    Update and Fix Vendor UI
    Update and Fix Vendor UI

--- resources/views/admin/vendor/create.blade.php
+++ resources/views/admin/vendor/create.blade.php
@@ -6,35 +6,46 @@
         name (required)
         phone (required)
         email (required)
-        website
+        website 
         address(required)
         --}}
-        <div class="col-6 offset-3 mt-5">
-            <form class="shadow-sm">
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Name</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Phone</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Email</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Email">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Website</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Website">
-                    </div>
+        <div class="col-9 offset-1 mt-5">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Create Vendor | <a href="{{ route('admin#vendor') }}">View All Vendor </a> </h3>
                 </div>
-                <!-- /.card-body -->
+                <form>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Name</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Phone</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Email</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Website</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1"
+                                placeholder="Enter Website">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Address</label>
+                            <textarea class="form-control" id="exampleInputPassword1" placeholder="Enter Address"></textarea>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
 
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
+    </div>
     </div>
 @endsection
